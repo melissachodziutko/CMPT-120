8b663f1dab35c32999eb1b91374dd960e29aa830
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2025-02-03 14:25:15 -0500

    Update if_else_input_practice.py

--- Assignment 2/if_else_input_practice.py
+++ Assignment 2/if_else_input_practice.py
@@ -1,3 +1,10 @@
+#This is exactly the same as the other file: however, now you specifically need to ask the user for input instead of setting the variable to 10.
 
+#The first if statement should read: if 10 (but use our variable, not the number 10) is greater than 12, print out "10 is greater than 12"
+#The second if/else should read: Else if 10 is greater than 11, print out "10 is greater than 11"
+#The third if/else should read: Else if 10 is equal to 10, print out "10 is equal to 10"
+#The else should read: Else print out "10 is less than 10"
+
+def main():
 
 main()
